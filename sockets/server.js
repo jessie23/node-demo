73a53252674684af0b95834eddec7672d6394ca2
Jessie Peterson <jessie23@uw.edu>
2015-12-03 17:16:47 -0800

    server code

--- sockets/server.js
+++ sockets/server.js
@@ -3,3 +3,48 @@
     main server script for the socket.io chat demo
 */
 
+var net = require('net');
+
+//create a new network server
+var server = net.createServer();
+
+//array of connected clients
+var clients = [];
+
+server.on('connection', function(socket) {
+   var name;
+
+    function broadcase(name, message) {
+        clients.forEach(function(client) {
+            if (client !== socket) {
+                client.write('[' + name + ']' + message);
+            }
+        });
+    }
+
+   clients.push(socket);
+
+   socket.write('Hello! What is your name?\n');
+
+   socket.on('data', function(data) {
+       if (!name) {
+           name = data.toString().trim();
+           socket.write('Hello ' + name + '!\n');
+       }
+       else {
+           var message = data.toString();
+           if (message.trim() === 'exit') {
+               socket.end();
+           }
+           broadcase(name, data.toString());
+       }
+
+       socket.end();
+   });
+});
+
+server.on('listening', function() {
+    console.log('server listening on port 3000');
+});
+
+server.listen(3000);
